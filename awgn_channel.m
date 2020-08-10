5ed9e5106d8bdb565d27db25d3586bf852bded9a
MahmudulAlam <mdmahmudulalam007@gmail.com>
2020-08-10 15:29:47 +0600

    cleaning code

--- awgn_channel.m
+++
@@ -1,9 +0,0 @@
-function received = awgn_channel(modulated, snr)
-% INPUT: 
-%   modulated: transmitted modulated signal
-%   snr: value of the signal to noise ratio 
-% OUTPUT: 
-%   received: noise added modulated signal that appears at the receiver
-
-received = awgn(modulated, snr, 'measured'); 
-end
